e75a73d82bc8c36d486115e5db1092fa7f68b1e5
Isac Osvaldo de Venancio Daniel <iovdidaniel@gmail.com>
2023-01-14 11:11:53 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 //se a nossa aplicacao estiver em modo de produção
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI:'mongodb+srv://root:root@cluster0.yvjwuvz.mongodb.net/?retryWrites=true&w=majority'}
+    module.exports = {mongoURI:'mongodb+srv://root:root@cluster0.yvjwuvz.mongodb.net/blog'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blog'}
 }
